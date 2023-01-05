cfc09b07c64b0e994c81f08d676d9d36cc57da1b
Yet <78223322+Yetity@users.noreply.github.com>
2023-01-05 17:06:35 +0000

    added new /db/ endpoint

--- index.js
+++ index.js
@@ -7,40 +7,32 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const bcrypt = require('bcrypt');
 
-async function hashPassword(password) {
-  try {
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    console.log("hased pass" + hashedPassword);
-    return hashedPassword;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
 
-async function verifyPassword(password, hashedPassword) {
-  const isMatch = await bcrypt.compare(password, hashedPassword);
-  return isMatch;
-}
-
-async function saveUser(id, name, pass) {
-  const hashedPassword = await hashPassword(pass);
+function saveUser(id, name, pass) {
   var uI = {
     "username" : name,
-    "password" : hashedPassword
+    "password" : pass
   }
 
   users.set(id.toString(), uI);
 }
 
 
-function getUser(id) {
+function getUser(id, info) {
   if (udb.hasOwnProperty(id)) {
-    var out = {
-      "username" : udb[id]["username"]
+    if (info == "name") {
+      var out = {
+        "username" : udb[id]["username"]
+      }
+    } else if ( info == "pass") {
+      var out = {
+        "password" : udb[id]["password"]
+      }
+    } else {
+      var out = {
+        "Error" :"What the hell are you trying to do with this function?"
+      }
     }
     return JSON.stringify(out)
   }
@@ -50,7 +42,7 @@ function getUser(id) {
 // defining the Express app
 const app = express();
 // defining an array to work as the database (temporary solution)
-const ads = {title: 'ask openai what you were doing'}
+const ads = {'title': 'ask openai what you were doing'}
 
 app.use(helmet()); // adding Helmet to enhance your Rest API's security
 app.use(bodyParser.json()); // using bodyParser to parse JSON bodies into JS objects
@@ -62,31 +54,50 @@ app.get('/', (req, res) => {
 });
 
 app.get('/user/:id', function (req, res) { 
-  res.send(getUser(req.params.id))
+  res.send(getUser(req.params.id), "name")
 })
 
-app.get('/pswrd/:id', async function (req, res) { 
-  if (req.ip == '172.31.128.1') {
-    res.send({"Error" : "Invalid Credentials"});
+app.get('/pswrd/:id', function (req, res) { 
+  if (req.hostname == 'hlonipoole692@gmail.com') {
+    res.send({"Error" : "Invalid Credentials"})
   } else {
-    const id = req.params.id;
-    const password = req.query.password;
+    res.send(getUser(req.params.id, "pass"))
+  }
+})
 
-    if (!udb.hasOwnProperty(id)) {
-      res.send({"Error" : "User not found"});
-    } else {
-      const hashedPassword = udb[id]["password"];
-      const isMatch = await verifyPassword(password, hashedPassword);
+app.post('/db', (req, res) => {
+  const va = req.body
+  console.log(req.body)
+  if (va.auth != "gg") {
+    console.log(req.body.auth)
+    res.send({"Error" : "Invalid"})
+  } else {
+    try {
 
-      if (isMatch) {
-        res.send({"password" : hashedPassword});
+      if (udb.hasOwnProperty(va.user.id) ) {
+        res.send({"Error" : "ID already exists and is in use."})
       } else {
-        res.send({"Error" : "Incorrect password"});
+        res.send({"Success" : "Success, your code works YetnT!"})
       }
+      
+    } catch (err) {
+      
+      res.send({"Error" : "k"})
+      
     }
   }
-});
+})
 
+/*
+{
+  "auth" : "p",
+  "user" : {
+    "id" : "",
+    "username" : "Username"
+  }
+}
+
+*/
 // starting the server
 app.listen(3001, () => {
   console.log('listening on port 3001');
@@ -100,7 +111,7 @@ END OF SERVeR SIDE
 
 var rand = Math.floor(Math.random() * 999999999999) + 1;
 
-// saveUser(rand, "Joe", "PASSWORD!")
+// saveUser(rand, "Han", "PASSWORD!")
 
 /*
 console.log(udb["342691365464"].username)
